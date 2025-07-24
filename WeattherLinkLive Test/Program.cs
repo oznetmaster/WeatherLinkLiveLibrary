514f76fe745b61b25575d728a07e82319a5d56ed
oznetmaster <ncolvin@marvelous.com>
2025-07-24 06:10:06 +0100

    Update nullability

--- WeattherLinkLive Test/Program.cs
+++ WeattherLinkLive Test/Program.cs
@@ -1,32 +1,34 @@
 ﻿using log4net.Config;
+
 using System;
 using System.Threading.Tasks;
+
 using WeatherLinkLive;
 
 namespace WeatherLinkLive_Test
-{
-    class Program
-    {
-        static async Task Main(string[] args)
-        {
-            BasicConfigurator.Configure();
+	{
+	class Program
+		{
+		static async Task Main (string[] args)
+			{
+			BasicConfigurator.Configure ();
 
-            var wll = new WeatherLinkLiveAPI.WeatherLinkLive("192.168.8.198", 10, 30, true);
-            await wll.InitializeAsync();
+			var wll = new WeatherLinkLiveAPI.WeatherLinkLive ("192.168.8.198", 10, 30, true);
+			await wll.InitializeAsync ();
 
-            do
-            {
-                await wll.RefreshAsync();
-                Console.WriteLine($"Current Temperature = {wll.Temperature:F1}°C");
-                Console.WriteLine($"Current Humidity = {wll.Humidity:F1}%");
-                Console.WriteLine($"Current Wind Chill = {wll.WindChill:F1}°C");
-                Console.WriteLine($"Current Wind Speed = {wll.WindSpeedLast:F1} mph");
-                Console.WriteLine($"Current Wind Direction = {wll.WindDirectionLast:F2}°");
-                Console.WriteLine($"Current Rain Rate = {wll.RainRate:F1}\"/hr");
-                Console.WriteLine($"Rainfall last 24 hours = {wll.RainfallLast24Hours:F1}\"");
-                Console.WriteLine($"Barometer at sea level = {wll.BarometerAtSeaLevel:F2}\" Hg - {wll.BarometerTrend}");
-            }
-            while (Console.ReadKey().Key != ConsoleKey.Escape);
-        }
-    }
-}
+			do
+				{
+				await wll.RefreshAsync ();
+				Console.WriteLine ($"Current Temperature = {wll.Temperature:F1}°C");
+				Console.WriteLine ($"Current Humidity = {wll.Humidity:F1}%");
+				Console.WriteLine ($"Current Wind Chill = {wll.WindChill:F1}°C");
+				Console.WriteLine ($"Current Wind Speed = {wll.WindSpeedLast:F1} mph");
+				Console.WriteLine ($"Current Wind Direction = {wll.WindDirectionLast:F2}°");
+				Console.WriteLine ($"Current Rain Rate = {wll.RainRate:F1}\"/hr");
+				Console.WriteLine ($"Rainfall last 24 hours = {wll.RainfallLast24Hours:F1}\"");
+				Console.WriteLine ($"Barometer at sea level = {wll.BarometerAtSeaLevel:F2}\" Hg - {wll.BarometerTrend}");
+				}
+			while (Console.ReadKey ().Key != ConsoleKey.Escape);
+			}
+		}
+	}
